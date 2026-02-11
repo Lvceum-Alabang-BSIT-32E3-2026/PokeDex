653b19f1c4ed945d606ef80d3e0e31896c25ce00
Mary Jhazmine Javier <jmaryjhazmine13@gmail.com>
2026-02-11 15:30:49 +0800

    Add LoginDto, TokenResponseDto, UserResponseDto

---
+++ IdentityServerApi/DTOs/TokenResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentityServerApi.DTOs
+{
+    public class TokenResponseDto
+    {
+        public string Token { get; set; } = null!;
+        public DateTime Expiration { get; set; }
+    }
+}
---
+++ IdentityServerApi/DTOs/UserResponseDto.cs
@@ -0,0 +1,10 @@
+namespace IdentityServerApi.DTOs
+{
+    public class UserResponseDto
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
