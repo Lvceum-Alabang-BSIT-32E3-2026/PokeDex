32c12dc63ffbfe386daebf42db3fdd91c5d36b7c
ninalamo <nin.alamo@outlook.com>
2026-02-11 17:10:49 +0800

    Revert "Add LoginDto, TokenResponseDto, UserResponseDto"
    This reverts commit 653b19f1c4ed945d606ef80d3e0e31896c25ce00.

--- IdentityServerApi/DTOs/TokenResponseDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace IdentityServerApi.DTOs
-{
-    public class TokenResponseDto
-    {
-        public string Token { get; set; } = null!;
-        public DateTime Expiration { get; set; }
-    }
-}
