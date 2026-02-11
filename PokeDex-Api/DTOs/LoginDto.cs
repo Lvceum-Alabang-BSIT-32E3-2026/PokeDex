f78ee8f58ed007e9f4ff8a2cc86b80b584334b3e
Mary Jhazmine Javier <jmaryjhazmine13@gmail.com>
2026-02-11 10:59:15 +0800

    feat(auth): create LoginDto and TokenResponseDto (#16)

---
+++ PokeDex-Api/DTOs/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PokeDex_Api.DTOs
+{
+    public class LoginDto
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
---
+++ PokeDex-Api/DTOs/TokenResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PokeDex_Api.DTOs
+{
+    public class TokenResponseDto
+    {
+        public required string Token { get; set; }
+        public required string User { get; set; }
+    }
+}
