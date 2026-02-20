c6003b343ef8a89c690d1fdeb7111d2fb743598d
JBLOBRICO <youremail@example.com>
2026-02-20 23:54:22 +0800

    commit

--- IdentityServerApi/DTOs/ChangePasswordDto.cs
+++ IdentityServerApi/DTOs/ChangePasswordDto.cs
@@ -8,8 +8,7 @@ namespace IdentityServerApi.DTOs
         public string CurrentPassword { get; set; }
 
         [Required]
-        [MinLength(6)] 
+        [MinLength(8)] // Updated from 6 to 8 to match Program.cs config
         public string NewPassword { get; set; }
     }
 }
-}   
