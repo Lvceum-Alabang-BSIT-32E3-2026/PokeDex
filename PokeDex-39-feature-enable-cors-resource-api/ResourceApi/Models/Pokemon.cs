972106961de977a7886aa0a98661ba17eae14a61
Jhiii0 <c2027-23@itmlyceumalabang.onmicrosoft.com>
2026-02-27 02:41:03 +0800

    Task 2.1.10: Enable CORS for Resource API

---
+++ PokeDex-39-feature-enable-cors-resource-api/ResourceApi/Models/Pokemon.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+
+namespace ResourceApi.Models;
+
+public class Pokemon
+{
+    public int Id { get; set; }
+    public int PokedexNumber { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+    public int Generation { get; set; }
+    public bool IsLegendary { get; set; }
+    public bool IsMythical { get; set; }
+    public int BaseExperience { get; set; }
+    public decimal Height { get; set; } // in meters
+    public decimal Weight { get; set; } // in kg
+
+    // Task 2.3.1: Base Stats Fields - Idinagdag para sa requirements ng task
+    public int HP { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+    public int SpecialAttack { get; set; }
+    public int SpecialDefense { get; set; }
+    public int Speed { get; set; }
+
+    // Navigation property
+
+    // Siguraduhin na ang 'PokemonType' class ay nage-exist sa iyong project
+
+    // Make sure the class 'PokemonType' is defined elsewhere in your project!
+    // Sa loob ng Pokemon class
+
+    public ICollection<PokemonType> PokemonTypes { get; set; } = new List<PokemonType>();
+}
