0cf832e8b0ed8a54bcbbb9d4d2419e9438c86912
Daniel Rafael Red <danred206@gmail.com>
2026-02-27 15:38:47 +0800

    Task 2.1.3: Create Type Entity

---
+++ Migrations/20260224053029_InitialCreateFinal.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResourceApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateFinal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Pokemons",
+                newName: "ImageUrl");
+
+            migrationBuilder.RenameColumn(
+                name: "BaseExperience",
+                table: "Pokemons",
+                newName: "PokedexNumber");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Generation",
+                table: "Pokemons",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLegendary",
+                table: "Pokemons",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMythical",
+                table: "Pokemons",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "PokemonType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PokemonType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PokemonPokemonType",
+                columns: table => new
+                {
+                    PokemonTypesId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PokemonsId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PokemonPokemonType", x => new { x.PokemonTypesId, x.PokemonsId });
+                    table.ForeignKey(
+                        name: "FK_PokemonPokemonType_PokemonType_PokemonTypesId",
+                        column: x => x.PokemonTypesId,
+                        principalTable: "PokemonType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PokemonPokemonType_Pokemons_PokemonsId",
+                        column: x => x.PokemonsId,
+                        principalTable: "Pokemons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PokemonPokemonType_PokemonsId",
+                table: "PokemonPokemonType",
+                column: "PokemonsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PokemonPokemonType");
+
+            migrationBuilder.DropTable(
+                name: "PokemonType");
+
+            migrationBuilder.DropColumn(
+                name: "Generation",
+                table: "Pokemons");
+
+            migrationBuilder.DropColumn(
+                name: "IsLegendary",
+                table: "Pokemons");
+
+            migrationBuilder.DropColumn(
+                name: "IsMythical",
+                table: "Pokemons");
+
+            migrationBuilder.RenameColumn(
+                name: "PokedexNumber",
+                table: "Pokemons",
+                newName: "BaseExperience");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Pokemons",
+                newName: "Type");
+        }
+    }
+}
